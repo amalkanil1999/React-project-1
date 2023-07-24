18dfc2e09324db06e3a375095ee65b94d3cdae1c
amalkanil1999 <amalkanilkolakot@gmail.com>
2023-07-24 17:57:40 +0530

    Graph section has been completed

--- src/screens/components/Dashboard.js
+++ src/screens/components/Dashboard.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import React from "react";
 import { styled } from "styled-components";
 import Popup from "./popup/Popup";
+import Chart from "react-apexcharts"
 
 function Dashboard(props) {
   const [selectedOption, setSelectedOption] = useState("");
@@ -10,6 +11,272 @@ function Dashboard(props) {
     console.log(selectedOption);
   };
   const [openPopup, setopenPopup] = useState(false);
+  const data = {
+    series: [
+      {
+        name: "Income",
+        data: [1.7,0.97,1.2,0.72,1.45,3.1,0.11,0.8,1.5,1.8],
+      },
+      {
+        name: "Saving",
+        data: [0.9,1.9,1.0,2.2,1.3,1.4,2.6,1.5,1.5,1.8],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        toolbar: {
+          show: true,
+        },
+      },
+      colors: ["#008080", "#deb887"],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"], 
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 1,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct"],
+        title: {
+          text: "Month",
+        },
+      },
+      yaxis: {
+        min: 0.01,
+        max: 3.30,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5,
+      },
+    },
+  };
+  const data1 = {
+    series: [
+      {
+        name: "Income",
+        data: [1.7,0.97,3.2,2.72,0.45,3.1,0.9,3.8,0.5,2.9],
+      },
+      {
+        name: "Saving",
+        data: [0.4,1.9,1.0,2.2,3.3,1.4,0.6,1.5,3.5,1.2],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        toolbar: {
+          show: true,
+        },
+      },
+      colors: ["#008080", "#deb887"],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"], 
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 1,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct"],
+        title: {
+          text: "Month",
+        },
+      },
+      yaxis: {
+        min: 0.01,
+        max: 3.30,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5,
+      },
+    },
+  };
+  const data2 = {
+    series: [
+      {
+        name: "Income",
+        data: [1.7,0.97,3.9,2.72,0.45,3.1,0.9,2.8,0.5,0.9],
+      },
+      {
+        name: "Saving",
+        data: [3.4,1.9,1.0,0.2,3.3,1.7,3.6,1.5,2.5,1.2],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        toolbar: {
+          show: true,
+        },
+      },
+      colors: ["#008080", "#deb887"],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"],
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 1,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct"],
+        title: {
+          text: "Month",
+        },
+      },
+      yaxis: {
+        min: 0.01,
+        max: 4,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5,
+      },
+    },
+  };
+  const data3 = {
+    series: [
+      {
+        name: "Income",
+        data: [2.2,2.97,0.2,3.72,2.45,0.1,1.11,2.8,0.5,3.8],
+      },
+      {
+        name: "Saving",
+        data: [0.9,3.9,2.0,1.2,3.3,0.4,2.6,3.5,0.5,3.8],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        toolbar: {
+          show: true,
+        },
+      },
+      colors: ["#008080", "#deb887"],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"], 
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 1,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct"],
+        title: {
+          text: "Month",
+        },
+      },
+      yaxis: {
+        min: 0.01,
+        max: 4,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5,
+      },
+    },
+  };
+  const getChartData = () => {
+    switch (selectedOption) {
+      case "2021":
+        return data;
+      case "2022":
+        return data1;
+      case "2023":
+        return data2;
+      case "2024":
+        return data3;
+      default:
+        return data; 
+    }
+  };
   return (
     <Main>
       <Top>
@@ -78,7 +345,7 @@ function Dashboard(props) {
           </Down>
         </Leftcontainer>
         <Rightcontainer>
-          <Graphheading>
+        <Graphheading>
             <H8>Analytics</H8>
             <Dot>
               <Smallsquareblack>
@@ -91,9 +358,12 @@ function Dashboard(props) {
               </Smallsquaregreen>
             </Dot>
           </Graphheading>
-          <GraphContainer>
-            <Graphimage src={require("../../Assets/graph.png")} />
-          </GraphContainer>
+          <Chart
+            options={getChartData().options}
+            series={getChartData().series}
+            type="line"
+            height={350}
+          />
         </Rightcontainer>
       </Middle>
       <Footer>
